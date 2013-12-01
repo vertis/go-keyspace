be75c8bc11a017fca00bede19dae6bcc0075afac
Luke Chadwick <me@vertis.io>
2013-12-01 12:09:20 +1100

    Initialize the array to zero

--- keyspace.go
+++ keyspace.go
@@ -3,11 +3,15 @@ package keyspace
 type Keyspace struct{
 	space []byte
 	length int
+	current []int
 }
 
 func New(space []byte, length int) Keyspace {
 	var k Keyspace
     k.space = space
     k.length = length
+
+    k.current = make([]int, length)
+    for i := range k.current  { k.current[i] = 0 }
     return k
 }
