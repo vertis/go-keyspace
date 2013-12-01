ea1d2b6f1705557df7053eb6c386718a7254e75c
Luke Chadwick <me@vertis.io>
2013-12-01 11:49:06 +1100

    Start refactoring

--- keyspace.go
+++ keyspace.go
@@ -1,43 +1,13 @@
-package main
+package keyspace
 
-import (
-		"fmt"
-)
-
-var keyspace = []byte("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
-
-
-func toKeyspaceString(arr []int) {
-	x := make([]byte, len(arr))
-    for idx, value := range arr {
-      x[idx]=keyspace[value]
-      //fmt.Println("str: ", value)
-    }
-    fmt.Println("str: ", string(x[:]))
+type Keyspace struct{
+	space []byte
+	length int
 }
 
-func incrementString(x * []int, offset int) {
-	arr := *x
-	if(arr[offset]==61) {
-		arr[offset]=0
-		incrementString(x, offset-1)
-	} else {
-		arr[offset]++
-	}
-	*x = arr
-}
-
-func main() {
-	start := []int{ 0, 0, 0, 0, 0, 0, 0, 0}
-	//toKeyspaceString(start)  
-    // 218340105584895
-    for i := 0; i < 1000000000; i++ {
-      if(i % 1000000000 == 0) {
-        fmt.Println(i)
-      }
-      incrementString(&start, len(start)-1)
-    }
-	toKeyspaceString(start)
-	//x := [...]byte { 48 }
-	//fmt.Println("Key: ", string(x[:])) //, reflect.TypeOf(dk))
+func New(space []byte, length int) Keyspace {
+	var k Keyspace
+    k.space = space
+    k.length = length
+    return k
 }
