43ce41d3b4f3e084b66b8920531b553ba98a88bc
Luke Chadwick <me@vertis.io>
2013-12-01 12:26:51 +1100

    Enable iterating through the keyspace

--- keyspace.go
+++ keyspace.go
@@ -6,6 +6,26 @@ type Keyspace struct{
 	current []int
 }
 
+func (k * Keyspace) Next() []byte {
+	k.incrementString(k.length-1)
+	x := make([]byte, k.length)
+	for idx, value := range k.current {
+      x[idx]=k.space[value]
+    }
+    return x
+}
+
+func (k * Keyspace) incrementString(offset int) {
+	x := k.current
+	if(x[offset]==len(k.space)-1) {
+		x[offset]=0
+		k.incrementString(offset-1)
+	} else {
+		x[offset]++
+	}
+	k.current = x
+}
+
 func New(space []byte, length int) Keyspace {
 	var k Keyspace
     k.space = space
